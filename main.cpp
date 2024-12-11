2a1eb15bba24d3e1b2abeae12e801b09165db6a1
Carles Gradolí Montagud <145761532+Carlessgm@users.noreply.github.com>
2024-12-11 18:33:34 +0100

    Actualización main.cpp para Prueba Ranking
    - Se ha modificado main.cpp para que cree 3 usuarios diferentes con 3 puntuaciones diferentes para comprobar el correcto funcionamiento del ranking

--- main.cpp
+++ main.cpp
@@ -11,8 +11,12 @@ int main(int argc, char *argv[])
     Connect4& game = Connect4::getInstance();
 
     Player* player = game.registerPlayer("Player1", "player1@example.com", "Password123!", QDate(1990, 1, 1), 100);
-    if (player) {
+    Player* player2 = game.registerPlayer("Player2", "player2@example.com", "Password123!", QDate(1990, 1, 1), 150);
+    Player* player3 = game.registerPlayer("Player3", "player3@example.com", "Password123!", QDate(1990, 1, 1), 50);
+    if (player && player2 && player3) {
         qDebug() << "Jugador registrado:" << player->getNickName();
+        qDebug() << "Jugador registrado:" << player2->getNickName();
+        qDebug() << "Jugador registrado:" << player3->getNickName();
     }
 
     MainWindow w;
