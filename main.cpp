ad54af72a4944fa9a46bdba05dc5041d9b7658f1
Pablo Utrillas Gonzalez <putrgon@upv.edu.es>
2025-01-08 19:14:21 +0100

    Agregados comentarios en main.cpp para mejor entendimiento del codigo

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@ int main(int argc, char *argv[])
     // Usar la biblioteca Connec4Lib
     Connect4& game = Connect4::getInstance();
 
+    // Registra algunos jugadores de ejemplo
     Player* player = game.registerPlayer("Player1", "player1@example.com", "Password123!", QDate(1990, 1, 1), 100);
     Player* player2 = game.registerPlayer("Player2", "player2@example.com", "Password123!", QDate(1990, 1, 1), 150);
     Player* player3 = game.registerPlayer("Player3", "player3@example.com", "Password123!", QDate(1990, 1, 1), 50);
@@ -21,6 +22,7 @@ int main(int argc, char *argv[])
 
     MainWindow w;
     QFile file(":/estilos/estilos.qss"); // Asegúrate de que el archivo esté en el recurso
+    // Intenta cargar la hoja de estilo desde los recursos
     if (file.open(QFile::ReadOnly)) {
         QString styleSheet = QLatin1String(file.readAll());
         //w.setStyleSheet(styleSheet);
